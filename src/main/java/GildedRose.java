c1569b1b4da5d7baabc0572d84e209283cf93a0d
Hugo-C <hchastel@laposte.net>
2019-02-04 17:00:10 +0100

    Added tests and created a constructor for GildedRose

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -1,10 +1,18 @@
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class GildedRose {
 
-	private static List<Item> items = null;
+	ArrayList<Item> items = new ArrayList<>();
+
+	public GildedRose(){
+        items.add(new Item("+5 Dexterity Vest", 10, 20));
+        items.add(new Item("Aged Brie", 2, 0));
+        items.add(new Item("Elixir of the Mongoose", 5, 7));
+        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        items.add(new Item("Conjured Mana Cake", 3, 6));
+    }
 
 	/**
 	 * @param args
@@ -12,21 +20,14 @@ public class GildedRose {
 	public static void main(String[] args) {
 		
         System.out.println("OMGHAI!");
-		
-        items = new ArrayList<Item>();
-        items.add(new Item("+5 Dexterity Vest", 10, 20));
-        items.add(new Item("Aged Brie", 2, 0));
-        items.add(new Item("Elixir of the Mongoose", 5, 7));
-        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        items.add(new Item("Conjured Mana Cake", 3, 6));
 
-        updateQuality();
+        GildedRose gildedRose = new GildedRose();
+        gildedRose.updateQuality();
 }
 
 
 	
-    public static void updateQuality()
+    public void updateQuality()
     {
         for (int i = 0; i < items.size(); i++)
         {
--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,12 +1,171 @@
-import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class GildedRoseTest {
 
+	private GildedRose gildedRose;
+
+	@Before
+	public void init(){
+		gildedRose = new GildedRose();
+
+	}
+
 	@Test
-	public void testTheTruth() {
-		assertTrue(true);
+	public void testSellIn() {
+		int sellIn = 10;
+		Item item = new Item("+5 Dexterity Vest", sellIn, 20);
+		gildedRose.items.add(item);
+
+		gildedRose.updateQuality();
+
+		assertEquals(item.sellIn, sellIn - 1);
+	}
+
+	@Test
+	public void testQualityWithSellInPositive() {
+		int quality = 10;
+		Item item = new Item("+5 Dexterity Vest", 10, quality);
+		gildedRose.items.add(item);
+
+		gildedRose.updateQuality();
+
+		assertEquals(item.quality, quality - 1);
+	}
+
+	@Test
+	public void testQualityWithSellInNegative() {
+		int quality = 10;
+		Item item = new Item("+5 Dexterity Vest", -10, quality);
+		gildedRose.items.add(item);
+
+		gildedRose.updateQuality();
+
+		assertEquals(item.quality, quality - 2);
+	}
+
+	@Test
+	public void testQualityIsPositive() {
+		int quality = 0;
+		Item item = new Item("+5 Dexterity Vest", -10, quality);
+		gildedRose.items.add(item);
+
+		gildedRose.updateQuality();
+
+		assertEquals(item.quality, quality);
+	}
+
+	@Test
+	public void testQualityAgedBrieWithSellInPositive() {
+		int quality = 5;
+		Item agedBrie = new Item("Aged Brie", 2, quality);
+		gildedRose.items.add(agedBrie);
+
+		gildedRose.updateQuality();
+
+		assertEquals(agedBrie.quality, quality + 1);
+	}
+
+	@Test
+	public void testQualityAgedBrieWithSellInNegative() {
+		int quality = 5;
+		Item agedBrie = new Item("Aged Brie", -2, quality);
+		gildedRose.items.add(agedBrie);
+
+		gildedRose.updateQuality();
+
+		assertEquals(agedBrie.quality, quality + 2);
+	}
+
+	@Test
+	public void testQualityIsUnder51WithSellInPositive() {
+		int quality = 50;
+		Item agedBrie = new Item("Aged Brie", 10, quality);
+		gildedRose.items.add(agedBrie);
+
+		gildedRose.updateQuality();
+
+		assertEquals(agedBrie.quality, 50);
+	}
+
+	@Test
+	public void testQualityIsUnder51WithSellInNegative() {
+		int quality = 49;
+		Item agedBrie = new Item("Aged Brie", -5, quality);
+		gildedRose.items.add(agedBrie);
+
+		gildedRose.updateQuality();
+
+		assertEquals(agedBrie.quality, 50);
+	}
+
+	@Test
+	public void testSellinAndQualitySulfuras() {
+		int quality = 80;
+		Item sulfuras =	new Item("Sulfuras, Hand of Ragnaros", 0, quality);
+		gildedRose.items.add(sulfuras);
+
+		gildedRose.updateQuality();
+
+		assertEquals(sulfuras.sellIn, 0);
+		assertEquals(sulfuras.quality, quality);
+	}
+
+	@Test
+	public void testQualityBackstageWithSellInOver10() {
+		int quality = 10;
+		Item backstage = new Item("Backstage passes to a TAFKAL80ETC concert", 15, quality);
+		gildedRose.items.add(backstage);
+
+		gildedRose.updateQuality();
+
+		assertEquals(backstage.quality, quality + 1);
+	}
+
+	@Test
+	public void testQualityBackstageWithSellInBetween10and5Excluded() {
+		int quality = 10;
+		Item backstage = new Item("Backstage passes to a TAFKAL80ETC concert", 6, quality);
+		gildedRose.items.add(backstage);
+
+		gildedRose.updateQuality();
+
+		assertEquals(backstage.quality, quality + 2);
+	}
+
+	@Test
+	public void testQualityBackstageWithSellInBetween5and0Excluded() {
+		int quality = 10;
+		Item backstage = new Item("Backstage passes to a TAFKAL80ETC concert", 2, quality);
+		gildedRose.items.add(backstage);
+
+		gildedRose.updateQuality();
+
+		assertEquals(backstage.quality, quality + 3);
+	}
+
+	@Test
+	public void testQualityBackstageWithSellInNegativeOrZero() {
+		int quality = 10;
+		Item backstage = new Item("Backstage passes to a TAFKAL80ETC concert", -10, quality);
+		gildedRose.items.add(backstage);
+
+		gildedRose.updateQuality();
+
+		assertEquals(backstage.quality, 0);
+	}
+
+	@Test
+	public void testQualityConjured() {  // TODO check conjured doesn't drop under 0 quality
+		int quality = 10;
+		Item backstage = new Item("Conjured Mana Cake", 3, quality);
+		gildedRose.items.add(backstage);
+
+		gildedRose.updateQuality();
+
+		assertEquals(backstage.quality, quality - 2);
 	}
 }
